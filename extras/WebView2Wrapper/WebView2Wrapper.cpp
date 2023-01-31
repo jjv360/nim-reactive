db94f8c0be2b05db331853bf6a0e93a25c8db692
Josh Fox <jjv360@me.com>
2023-01-31 18:22:04 +0200

    WebView2Wrapper bounds + navigate

--- extras/WebView2Wrapper/WebView2Wrapper.cpp
+++ extras/WebView2Wrapper/WebView2Wrapper.cpp
@@ -1,11 +1,13 @@
 //
 // This is a simple wrapper around WebView2 which allows usage from Nim.
 
+#include <windows.h>
+#include <stdlib.h>
+#include <string>
+#include <tchar.h>
 #include <wrl.h>
 #include <wil/com.h>
 #include <atlstr.h>
-// <IncludeHeader>
-// include WebView2 header
 #include "WebView2.h"
 
 using namespace Microsoft::WRL;
@@ -67,5 +69,64 @@ extern "C" __declspec(dllexport) void WebView2_CreateController(HWND parentWindo
 			return S_OK;
 
 		}).Get());
+
+}
+
+// Set WebView bounds within the parent window
+extern "C" __declspec(dllexport) void WebView2_SetBounds(ICoreWebView2Controller* controller, long x, long y, long width, long height) {
+	RECT bounds;
+	bounds.left = x;
+	bounds.top = y;
+	bounds.right = width - x;
+	bounds.bottom = height = y;
+	controller->put_Bounds(bounds);
+}
+
+// Convert UTF8 string to UTF16
+// Source: https://stackoverflow.com/a/148766/1008736
+std::wstring UTF8_to_wchar(const char* in)
+{
+	std::wstring out;
+	unsigned int codepoint;
+	while (*in != 0)
+	{
+		unsigned char ch = static_cast<unsigned char>(*in);
+		if (ch <= 0x7f)
+			codepoint = ch;
+		else if (ch <= 0xbf)
+			codepoint = (codepoint << 6) | (ch & 0x3f);
+		else if (ch <= 0xdf)
+			codepoint = ch & 0x1f;
+		else if (ch <= 0xef)
+			codepoint = ch & 0x0f;
+		else
+			codepoint = ch & 0x07;
+		++in;
+		if (((*in & 0xc0) != 0x80) && (codepoint <= 0x10ffff))
+		{
+			if (sizeof(wchar_t) > 2)
+				out.append(1, static_cast<wchar_t>(codepoint));
+			else if (codepoint > 0xffff)
+			{
+				out.append(1, static_cast<wchar_t>(0xd800 + (codepoint >> 10)));
+				out.append(1, static_cast<wchar_t>(0xdc00 + (codepoint & 0x03ff)));
+			}
+			else if (codepoint < 0xd800 || codepoint >= 0xe000)
+				out.append(1, static_cast<wchar_t>(codepoint));
+		}
+	}
+	return out;
+}
+
+// Navigate to page
+extern "C" __declspec(dllexport) void WebView2_Navigate(ICoreWebView2Controller* controller, const char* url) {
+
+	// Get WebView
+	wil::com_ptr<ICoreWebView2> webview;
+	controller->get_CoreWebView2(&webview);
+
+	// Navigate
+	auto wstr = UTF8_to_wchar(url);
+	webview->Navigate(wstr.c_str());
 
 }
