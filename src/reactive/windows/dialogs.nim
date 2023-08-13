6001959b9bd100bc13a57c6604d8ed2bc96cc352
jjv360 <jjv360@me.com>
2023-08-13 13:09:31 +0200

    Confirm Win32 dialog

--- src/reactive/windows/dialogs.nim
+++ src/reactive/windows/dialogs.nim
@@ -16,3 +16,20 @@ proc alert*(text: string, title: string = "", icon: ReactiveDialogIcon = dlgInfo
 
     # Show message box
     MessageBox(0, text, title, MB_OK or iconFlag)
+
+
+# Show a confirmation prompt
+proc confirm*(text: string, title: string = "", icon: ReactiveDialogIcon = dlgInfo) : bool =
+
+    # Get correct icon
+    var iconFlag: UINT = MB_ICONINFORMATION
+    if icon == ReactiveDialogIcon.dlgWarning: iconFlag = MB_ICONWARNING
+    if icon == ReactiveDialogIcon.dlgError: iconFlag = MB_ICONERROR
+    if icon == ReactiveDialogIcon.dlgQuestion: iconFlag = MB_ICONQUESTION
+
+    # Show message box
+    let res = MessageBox(0, text, title, MB_OKCANCEL or iconFlag)
+    if res == IDOK: 
+        return true
+    else: 
+        return false
